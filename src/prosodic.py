66ca41453d28c3a6e025b189cdb035b6ad61679f
Ilan Malkin <malkin.ilan@gmail.com>
2022-09-21 17:31:20 +0200

    Prosodic feature extraction for all files in the myprosody database folder.
    Note: the actual myprosody library is not used (except for the code copied into the source). But the database folder structure is required.

--- src/prosodic.py
+++ src/prosodic.py
@@ -3,10 +3,10 @@ import pickle
 import os
 import numpy as np
 import pandas as pd
-from tabulate import tabulate
+import re
 
 # fuction taken from myprosody library
-def run_praat_file(m, p):
+def run_praat_file(m: str, p: str) -> list[str] | None:
     """
     m : filename
     p : path to dataset folder
@@ -14,9 +14,9 @@ def run_praat_file(m, p):
 
     returns : objects outputed by the praat script
     """
-    sound = p+"/"+"dataset"+"/"+"audioFiles"+"/"+m+".wav"
-    sourcerun = p+"/"+"dataset"+"/"+"essen"+"/"+"myspsolution.praat"
-    path = p+"/"+"dataset"+"/"+"audioFiles"+"/"
+    sound: str = p+"/"+"dataset"+"/"+"audioFiles"+"/"+m+".wav"
+    sourcerun: str = p+"/"+"dataset"+"/"+"essen"+"/"+"myspsolution.praat"
+    path: str = p+"/"+"dataset"+"/"+"audioFiles"+"/"
 
     assert os.path.isfile(sound), "Wrong path to audio file"
     assert os.path.isfile(sourcerun), "Wrong path to praat script"
@@ -35,7 +35,7 @@ def run_praat_file(m, p):
         z3 = 0
         print("Try again the sound of the audio was not clear")
 
-def mysptotal(m, p):
+def mysptotal(m: str, p: str) -> pd.DataFrame:
     """
     Overview: returns dataframe of all values
     """
@@ -43,17 +43,39 @@ def mysptotal(m, p):
     z3 = np.array(z2)
     z4 = np.array(z3)[np.newaxis]
     z5 = z4.T
-    dataset = pd.DataFrame({"number_ of_syllables": z5[0, :], "number_of_pauses": z5[1, :], "rate_of_speech": z5[2, :], "articulation_rate": z5[3, :], "speaking_duration": z5[4, :],
+    dataset: pd.DataFrame = pd.DataFrame({"file": m[0 : 20], "number_ of_syllables": z5[0, :], "number_of_pauses": z5[1, :], "rate_of_speech": z5[2, :], "articulation_rate": z5[3, :], "speaking_duration": z5[4, :],
                         "original_duration": z5[5, :], "balance": z5[6, :], "f0_mean": z5[7, :], "f0_std": z5[8, :], "f0_median": z5[9, :], "f0_min": z5[10, :], "f0_max": z5[11, :],
                         "f0_quantile25": z5[12, :], "f0_quan75": z5[13, :]})
     return dataset
 
-curr_dirname = os.path.dirname(__file__)
-database_dirname = os.path.join(curr_dirname, '../myprosody/myprosody')
+def extract_prosodic_from_folder(p: str) -> pd.DataFrame:
+    """
+    Extracts prosodic feature from all .wav files in dataset
+    p: path to dataset folder
+    """
+    path: str = p+"/"+"dataset"+"/"+"audioFiles"+"/"
+    files: list[str] = os.listdir(path)
+    print(files)
+    wav_files: list[str] = [os.path.splitext(x)[0] for x in files if re.search(r'\.wav$', x) ]
+    print(wav_files)
+    features: list[pd.DataFrame] = [mysptotal(file, p) for file in wav_files]
+    feature_df = pd.concat(features, ignore_index=True)
+    return feature_df
 
-m="test" # Audio File title "
-# c=r"C:\Users\malki\Documents\Wits Programming\InvestigationProjectDataAndCode\test" # Path to the Audio_File directory (Python 3.7) 
-p=database_dirname # Path to the Audio_File directory (Python 3.7) 
 
-dataset = mysptotal(m, p)
-print(tabulate(dataset, headers = 'keys', tablefmt = 'psql'))
+
+
+
+def main():
+    curr_dirname = os.path.dirname(__file__)
+    database_dirname = os.path.join(curr_dirname, '../myprosody/myprosody')
+
+    # m: str = "test" # Audio File title "
+    p: str = database_dirname # Path to the Audio_File directory (Python 3.7) 
+
+    feature_df: pd.DataFrame = extract_prosodic_from_folder(p)
+    # feature_df: pd.DataFrame = mysptotal(m, p)
+    print(feature_df)
+
+if __name__ == "__main__":
+    main()
