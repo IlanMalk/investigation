5910694f77191f96d2564e26c35a140761fec1e0
Adam Blumenthal <adamblumenthal98@gmail.com>
2022-10-18 18:55:55 +0200

    tTestDBates

--- src/tTest.py
+++ src/tTest.py
@@ -24,10 +24,52 @@ data=pd.read_csv(pjs)
 f0=data.meanF0.tolist()
 #print(f0)
 
-l=stats.ttest_1samp(f0, popmean=200)
+l=stats.ttest_1samp(f0, popmean=130)
 print(l)
 
 hnr=data.hnr.tolist()
 
 h=stats.ttest_1samp(hnr, popmean=5)
 print(h)
+
+
+localJit=data.localJitter.tolist()
+
+localJitter=stats.ttest_1samp(localJit, popmean=0.027)
+print(localJitter)
+
+#Comparison to Dbates
+f0=data.meanF0.tolist()
+#print(f0)
+
+l=stats.ttest_1samp(f0, popmean=220)
+print(l)
+
+f0St=data.stdevF0.tolist()
+#print(f0)
+
+f0STDEV=stats.ttest_1samp(f0St, popmean=88.2)
+print(f0STDEV)
+
+
+hnr=data.hnr.tolist()
+
+h=stats.ttest_1samp(hnr, popmean=4.43)
+print(h)
+
+
+localJit=data.localJitter.tolist()
+
+localJitter=stats.ttest_1samp(localJit, popmean=0.027)
+print(localJitter)
+
+rapJitter=data.rapJitter.tolist()
+
+rapJitter1=stats.ttest_1samp(rapJitter, popmean=0.017)
+print(rapJitter1)
+
+ppq5Jitter=data.ppq5Jitter.tolist()
+
+ppq5Jitter1=stats.ttest_1samp(ppq5Jitter, popmean=0.0190)
+print(ppq5Jitter1)
+
