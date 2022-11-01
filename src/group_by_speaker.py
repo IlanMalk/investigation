cf234b179b8fd15ea5063da92ef209e2cc0b543a
Ilan Malkin <malkin.ilan@gmail.com>
2022-11-01 18:14:22 +0200

    PJS grouped by speaker: modified group_by_speaker.py and put results in results/ folder

--- src/group_by_speaker.py
+++ src/group_by_speaker.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import os
+import glob
 
 def group_by_speaker(feature_df):
-    speakers = [filename.split("_")[0] for filename in feature_df["filename"].tolist()]
-    feature_df["speaker"] = speakers
+    if "speaker" not in feature_df.columns:
+        speakers = [filename.split("_")[0] for filename in feature_df["filename"].tolist()]
+        feature_df["speaker"] = speakers
     grouped = feature_df.groupby(["speaker"]).mean()
     return grouped
 
 def main():
-    feature_df = pd.read_csv("../pitch_jitter_shimmer.csv")
+    csv_str = os.path.join(os.pardir, "results", "pjs-debates", "*.csv")
+    csvs = glob.glob(csv_str)
+    speech_dfs = [pd.read_csv(csv) for csv in csvs]
+    # feature_df = pd.read_csv("../pitch_jitter_shimmer.csv")
+    feature_df = pd.concat(speech_dfs, ignore_index=True)
     grouped = group_by_speaker(feature_df)
     print(grouped)
+    grouped.to_csv("../pjs-debates-grouped-speaker.csv")
+
 
 if __name__ == "__main__":
     main()
